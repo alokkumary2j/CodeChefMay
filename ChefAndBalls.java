39db6a1c2f3bbd52c1168b896173a8885bc6e17e
Alok Kumar <alokkumary2j@gmail.com>
2016-05-10 00:44:03 +0530

    Add files via upload

--- ChefAndBalls.java
+++ ChefAndBalls.java
@@ -9,9 +9,9 @@ class ChefAndBalls {
 	public static void main(String[] args) {
 		System.out.println(1);
 		System.out.flush();
-		System.out.println(3+" "+1+" "+2+" "+3);
+		System.out.println(3+" "+1+" "+2+" "+2);
 		System.out.flush();
-		System.out.println(1+" "+4);
+		System.out.println(3+" "+3+" "+3+" "+4);
 		System.out.flush();
 		BufferedReader cnsl=null;
 		cnsl= new BufferedReader(new InputStreamReader(System.in));
@@ -19,35 +19,25 @@ class ChefAndBalls {
 		try {
 			op=cnsl.readLine();
 			int output=Integer.parseInt(op);
-			if(output==2){
+			if(output==0){
 				System.out.println(2+"\n"+"5");
 				System.out.flush();
 			}
+			else if(output==2){
+				System.out.println(2+"\n"+"2");
+				System.out.flush();
+			}
 			else if(output==1){
+				System.out.println(2+"\n"+"1");
+				System.out.flush();
+			}
+			else if(output==-1){
 				System.out.println(2+"\n"+"4");
 				System.out.flush();
 			}
-			else{
-				//Candidates are 1,2,3
-				System.out.println(1);
+			else if(output==-2){
+				System.out.println(2+"\n"+"3");
 				System.out.flush();
-				System.out.println(1+" "+1);
-				System.out.flush();
-				System.out.println(1+" "+2);
-				System.out.flush();
-				int diff=Integer.parseInt(cnsl.readLine());
-				if(diff==0){
-					System.out.println(2+"\n"+"3");
-					System.out.flush();					
-				}
-				else if(diff==-1){
-					System.out.println(2+"\n"+"2");
-					System.out.flush();										
-				}
-				else{
-					System.out.println(2+"\n"+"1");
-					System.out.flush();
-				}
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
